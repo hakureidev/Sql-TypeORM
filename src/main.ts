0b8ffd5391bd19c27f89cbd8619f4b9918e4c531
norieau <a.arijogja@gmail.com>
2024-01-27 21:44:01 +0700

    print logging on console

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,6 @@ async function bootstrap() {
    */
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
